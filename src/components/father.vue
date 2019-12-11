ba207da0297eb0b5b52db82f6eac483eef820046
Moyo <Moyo203@163.com>
2019-12-11 20:02:14 +0800

    子级传递数据给父级

--- src/components/father.vue
+++ src/components/father.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="father">
       <h1>这里是父级</h1>
+      <h2>儿子告诉我他女朋友叫{{songfname}}</h2>
       <!-- 3、动态绑定，这里的myname是和子级的命名一致 -->
-      <son :myname='sonname'></son>
+      <!-- 重点@getname='dname'事件监听 -->
+      <son :myname='sonname' @getname='dname'></son>
+
   </div>
 </template>
 //一、父级传递数给子级，爸爸给儿子取名为小明
@@ -12,16 +15,25 @@ import son from '@/components/son.vue'
 export default {
   data () {
     return {
-      sonname: '小明'
+      sonname: '小明',
+      songfname: '??'
     }
   },
   // 2、注意要注册
   components: {
     son
+  },
+  methods: {
+    dname (data) {
+      this.songfname = data
+    }
   }
 }
 </script>
 
 <style>
-
+.father{
+background-color: #95cfb7;
+height: 500px;
+}
 </style>
--- src/components/son.vue
+++ src/components/son.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="son">
-      <h2>这里是子级</h2>
+      <h2>这里是子级1 -- 儿子</h2>
       <h2>爸爸给我取名为{{myname}}</h2>
+      <h2>我的女朋友叫{{mygfname}}</h2>
+      <!-- 点击触发事件 -->
+      <button @click="tellname">点击告诉爸爸女朋友名字</button>
+
   </div>
 </template>
-
+// 二、子级传递数据给父级
 <script>
 
 export default {
 // 接收数据
-  props: ['myname']
+  props: ['myname'],
+  data () {
+    return {
+      mygfname: '小红'
+    }
+  },
+  // 定义事件
+  methods: {
+    tellname () {
+      // 发射事件，并传递参数
+      this.$emit('getname', this.mygfname)
+    }
+  }
 }
 </script>
 
 <style>
-
+.son{
+  background-color: #ff823a;
+  color:#696758
+}
 </style>
